132f571a2c6ef5d21548b9b0790f797e07dd03d5
volynkina <bzzz18@yandex.ru>
2023-01-29 16:51:11 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
     }
+    public static void checkYear(int year) {
+
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+            System.out.println(year + " год - високосный год.");
+        } else {
+            System.out.println(year + " год - невисокосный год.");
+        }
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int year = 2021;
+        checkYear(year);
+        System.out.println();
+
+    }
+
 }
