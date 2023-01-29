33563613b0a1c6ad8f0a4a1a056def30fc5c1ea6
volynkina <bzzz18@yandex.ru>
2023-01-29 17:29:29 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
     }
     public static void checkYear(int year) {
@@ -19,4 +20,30 @@ public class Main {
 
     }
 
-}
+    public static void printTheNameOfVersion(int deviceOS, int deviceYear) {
+        if (deviceOS == 0) {
+            if (deviceYear >= 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите обычную версию приложения для iOS по ссылке");
+            }
+        } else if (deviceOS == 1) {
+            if (deviceYear >= 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите обычную версию приложения для iOS по ссылке");
+            }
+        } else {
+            System.out.println("Выберете операционную систему вашего устройства");
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        int clientDeviceOs = 1;
+        int clientDeviceYear = 2015;
+        printTheNameOfVersion(clientDeviceOs,clientDeviceYear);
+        System.out.println();
+    }
+
+    }
+
